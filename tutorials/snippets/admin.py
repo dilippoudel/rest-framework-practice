6401d3a0b74e22ca9f9fec26da76cbd911d4167c
dilippoudel <dilippoudel578@gmail.com>
2025-01-02 18:05:11 +0200

    Defined admin page for snippet

--- tutorials/snippets/admin.py
+++ tutorials/snippets/admin.py
@@ -1,3 +1,11 @@
+"""Django admin site for snippets."""
 from django.contrib import admin
 
-# Register your models here.
+from snippets.models import Snippet
+
+class SnippetAdmin(admin.ModelAdmin):
+    """Define the snippet pages."""
+    ordering = ['id']
+    list_display = ['title', 'language', 'created']
+
+admin.site.register(Snippet, SnippetAdmin)
