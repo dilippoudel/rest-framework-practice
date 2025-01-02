2e86875090e090eb635858a8c7d75758946c5880
dilippoudel <dilippoudel578@gmail.com>
2025-01-02 17:48:50 +0200

    Changed url for class based view

--- tutorials/snippets/urls.py
+++ tutorials/snippets/urls.py
@@ -3,7 +3,13 @@ from snippets import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('snippets/', views.snippet_list),
-    path('snippets/<int:pk>/', views.snippet_detail)
+    # url patterns while using function based views.
+    
+    # path('snippets/', views.snippet_list),
+    # path('snippets/<int:pk>/', views.snippet_detail),
+    
+    # url patterns while using class based views.
+    path('snippets/', views.SnippetList.as_view()),
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view())
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
